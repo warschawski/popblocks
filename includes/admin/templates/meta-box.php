6e0d51e9d716694e5aa7b14c8d768dc32ca8c848
Marc Espanol <marc.espanol@warschawski.com>
2025-12-01 14:07:12 -0500

    Styles adjusted

--- includes/admin/templates/meta-box.php
+++ includes/admin/templates/meta-box.php
@@ -58,22 +58,21 @@
 </script>
 
 <div
-  class="components-tab-panel"
+  class="popblocks-components-tab-panel"
   x-data="popupsMetaBox({
     color: null,
     trigger: '',
   })"
   x-on:tab-activated="onTabActivated"
 >
-
   <div
     x-data="popupsTabs({
       activeTab: 'trigger'
     })"
   >
-    <div class="components-tab-panel__tabs">
-      <div class="components-tab-panel__border"></div>
-      <button class="components-button components-tab-panel__tabs-item"
+    <div class="popblocks-components-tab-panel__tabs">
+      <div class="popblocks-components-tab-panel__border"></div>
+      <button class="popblocks-components-button popblocks-components-tab-panel__tabs-item"
         data-active-item="true"
         role="tab"
         aria-selected="true"
@@ -84,7 +83,7 @@
       >
         <span>Triggers</span>
       </button>
-      <button class="components-button components-tab-panel__tabs-item"
+      <button class="popblocks-components-button popblocks-components-tab-panel__tabs-item"
         data-active-item="true"
         role="tab"
         aria-selected="true"
@@ -95,7 +94,7 @@
       >
         <span>Behavior</span>
       </button>
-      <button class="components-button components-tab-panel__tabs-item"
+      <button class="popblocks-components-button popblocks-components-tab-panel__tabs-item"
         data-active-item="true"
         role="tab"
         aria-selected="true"
@@ -148,27 +147,26 @@
       x-data="PopUpsData.options"
     >
       <div
-        class="options_tab_wrapper"
-        
+        class="popblocks-options_tab_wrapper"
       >
         <div 
-          class="flag_toggle_wrapper components-flex components-h-stack"
+          class="popblocks-flag_toggle_wrapper"
         >
           <span
-            class="components-form-toggle"
+            class="popblocks-components-form-toggle"
             x-bind:class="{ 'is-checked': active }"
           >
             <input 
-              class="components-form-toggle__input"
+              class="popblocks-components-form-toggle__input"
               type="checkbox"
               id="cookie_flag"
               name="cookie_flag"
               x-model="active"
             >
-            <span class="components-form-toggle__track"></span>
-            <span class="components-form-toggle__thumb"></span>
+            <span class="popblocks-components-form-toggle__track"></span>
+            <span class="popblocks-components-form-toggle__thumb"></span>
           </span>
-          <label class="components-flex-item components-flex-block components-toggle-control__label" for="cookie_flag">
+          <label class="popblocks-components-toggle-control__label" for="cookie_flag">
             Cookie Flag
           </label>
         </div>
@@ -177,54 +175,53 @@
           class="cookie_settings"
           x-show="active"
         >
-          <div class="components-flex components-input-base">
-            <div class="components-flex-item">
+          <div class="popblocks-components-input-base">
+            <div>
               <label 
                 for="cookie-name" 
-                class="components-truncate components-text components-input-control__label"
+                class="popblocks-components-input-control__label"
               >
                 Cookie Name
               </label>
             </div>
             <div 
-              class="components-input-control__container"
+              class="popblocks-components-input-control__container"
             >
               <input  
                 id="cookie-name"
-                class="components-input-control__input"
+                class="popblocks-components-input-control__input"
                 type="text"
                 x-model="name"
                 x-bind:placeholder="name"
               />
-              <div aria-hidden="true" class="components-input-control__backdrop"></div>
+              <div aria-hidden="true" class="popblocks-components-input-control__backdrop"></div>
             </div>
           </div>
 
-          <div class="components-flex components-input-base">
-
-            <div class="components-flex-item">
+          <div class="popblocks-components-input-base">
+            <div>
               <label 
                 for="cookie-time" 
-                class="components-truncate components-text components-input-control__label"
+                class="popblocks-components-input-control__label"
               >
                 Cookie Time
               </label>
             </div>
             <div 
-              class="components-input-control__container"
+              class="popblocks-components-input-control__container"
             >
               <input  
                 id="cookie-time"
-                class="components-input-control__input"
+                class="popblocks-components-input-control__input"
                 type="number"
                 placeholder="0"
                 x-model="duration.value"
               />
               <span 
-                class="components-input-control__suffix"
+                class="popblocks-components-input-control__suffix"
               >
                 <select 
-                  class="components-unit-control__select" 
+                  class="popblocks-components-unit-control__select" 
                   aria-label="Select unit"
                   x-model="duration.unit"
                 >
@@ -234,7 +231,7 @@
                   <option value="months">months</option>
                 </select>
               </span>
-              <div aria-hidden="true" class="components-input-control__backdrop"></div>
+              <div aria-hidden="true" class="popblocks-components-input-control__backdrop"></div>
             </div>
 
           </div>
@@ -253,16 +250,16 @@
 
 <template id="popup_conditionals">
   <div>
-    <p x-show="gIndex !== 0" class="or_group_separator">or</p>
+    <p x-show="gIndex !== 0" class="popblocks-or_group_separator">or</p>
     <template x-for="(rule, rIndex) in group.rules" :key="rule.id">
-      <div class="popup_rule_group">
-        <div x-show="rule.rule === 'or'" class="or_group_separator">or</div>
-        <div class="popup_trigger_condition">
-          <div class="popup_condition_wrapper">
+      <div class="popblocks-popup_rule_group">
+        <div x-show="rule.rule === 'or'" class="popblocks-or_group_separator">or</div>
+        <div class="popblocks-popup_trigger_condition">
+          <div class="popblocks-popup_condition_wrapper">
 
-            <div class="components-input-control__container">
+            <div class="popblocks-components-input-control__container">
               <select 
-                class="components-select-control__input"
+                class="popblocks-components-select-control__input"
                 x-model="rule.type"
                 x-on:change="updateRuleType(rule)"
               >
@@ -277,8 +274,8 @@
                   ></option>
                 </template>
               </select>
-              <span class="components-input-control__suffix">
-                <div data-wp-component="InputControlSuffixWrapper" class="components-input-control-suffix-wrapper">
+              <span class="popblocks-components-input-control__suffix">
+                <div data-wp-component="InputControlSuffixWrapper" class="popblocks-components-input-control-suffix-wrapper">
                   <div>
                     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="18" height="18" aria-hidden="true" focusable="false">
                       <path d="M17.5 11.6L12 16l-5.5-4.4.9-1.2L12 14l4.5-3.6 1 1.2z"></path>
@@ -286,15 +283,15 @@
                   </div>
                 </div>
               </span>
-              <div aria-hidden="true" class="components-input-control__backdrop"></div>
+              <div aria-hidden="true" class="popblocks-components-input-control__backdrop"></div>
             </div>
 
             <div 
-              class="components-input-control__container"
+              class="popblocks-components-input-control__container"
               x-show="!['none', 'scroll'].includes(rule.operator)"
             >
               <select 
-                class="components-select-control__input"
+                class="popblocks-components-select-control__input"
                 x-model="rule.operator" 
               >
                 <template
@@ -307,8 +304,8 @@
                   ></option>
                 </template>
               </select>
-              <span class="components-input-control__suffix">
-                <div class="components-input-control-suffix-wrapper">
+              <span class="popblocks-components-input-control__suffix">
+                <div class="popblocks-components-input-control-suffix-wrapper">
                   <div>
                     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="18" height="18" aria-hidden="true" focusable="false">
                       <path d="M17.5 11.6L12 16l-5.5-4.4.9-1.2L12 14l4.5-3.6 1 1.2z"></path>
@@ -316,38 +313,38 @@
                   </div>
                 </div>
               </span>
-              <div aria-hidden="true" class="components-input-control__backdrop"></div>
+              <div aria-hidden="true" class="popblocks-components-input-control__backdrop"></div>
             </div>
 
             <template
               x-for="operatorOption in getOperatorOptions(rule)"
             >
               <div 
-                class="components-input-control__container"
+                class="popblocks-components-input-control__container"
                 x-show="rule.operator == operatorOption.id && rule.operator !== 'none'"
               >
                 <input  
-                  class="components-input-control__input"
+                  class="popblocks-components-input-control__input"
                   type="text"
                   x-model="rule.value"
                   x-bind:placeholder="operatorOption.placeholder"
                 />
                 <span 
-                  class="components-input-control__suffix"
+                  class="popblocks-components-input-control__suffix"
                 >
                   <div 
-                    class="components-unit-control__unit-label"
+                    class="popblocks-components-unit-control__unit-label"
                     x-text="rule.suffix" 
                     x-show="rule.suffix !== 'none' && activeTab !== 'behavior'"
                   ></div>
                 </span>
-                <div aria-hidden="true" class="components-input-control__backdrop"></div>
+                <div aria-hidden="true" class="popblocks-components-input-control__backdrop"></div>
               </div>
             </template>
 
           </div>
           <button
-            class="popup_condition_button and_button components-button is-primary"
+            class="popblocks-popup_condition_button popblocks-and_button popblocks-components-button popblocks-is-primary"
             x-on:click="createRule(gIndex, rIndex)"
             x-show="activeTab !== 'trigger'"
           >
@@ -356,7 +353,7 @@
           <button 
             type="button" 
             aria-disabled="false" 
-            class="popup_condition_remove components-button is-next-40px-default-size is-secondary is-destructive"\
+            class="popblocks-popup_condition_remove popblocks-components-button popblocks-is-secondary popblocks-is-destructive"
             x-show="
               (gIndex == 0 && group.rules.length > 1) ||
               (gIndex == 0 && $data[groupName].length > 1) ||
@@ -374,7 +371,7 @@
       x-show="gIndex == $data[groupName].length - 1"
     >
       <p>or</p>
-      <button class="popup_condition_button or_button components-button is-primary"
+      <button class="popblocks-popup_condition_button popblocks-or_button popblocks-components-button popblocks-is-primary"
         x-on:click="createGroup()"
       >
         Add New Group
